455395c0fb363193ae2dd0f8d4f53b11518c851d
joyoh666 <joeyoh666@gmail.com>
2026-07-13 17:18:11 +0900

    0712

---
+++ import torch.py
@@ -0,0 +1,5 @@
+import torch
+print("CUDA available:", torch.cuda.is_available())
+print("GPU count:", torch.cuda.device_count())
+if torch.cuda.is_available():
+    print("GPU:", torch.cuda.get_device_name(0))
